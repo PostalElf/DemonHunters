2d2a8b8f0976751c921186397e3033a5f7b60ba1
PostalElf <thanthos@gmail.com>
2016-06-08 04:45:38 +0800

    Added effectSpeed

---
+++ DemonHunters/Frames/Effect/EffectSpeed.vb
@@ -0,0 +1,33 @@
+﻿Public Class EffectSpeed
+    Inherits Effect
+    Friend Walk As Integer
+    Friend Stride As Integer
+    Friend Run As Integer
+
+    Friend Shared Function Build(ByVal raw As String) As EffectSpeed
+        Dim r As String() = raw.Split("/")
+
+        Dim total As New EffectSpeed
+        With total
+            total.Walk = r(0)
+            total.Stride = r(1)
+            total.Run = r(2)
+        End With
+        Return total
+    End Function
+    Friend Overrides Function Unbuild() As String
+        Return "EffectSpeed:" & Walk & "/" & Stride & "/" & Run
+    End Function
+    Public Overrides Function ToString() As String
+        Return "Speed:" & Walk & "/" & Stride & "/" & Run
+    End Function
+    Friend Overrides Sub Merge(ByVal effect As Effect)
+        If TypeOf effect Is EffectSpeed = False Then Exit Sub
+        Dim es As EffectSpeed = CType(effect, EffectSpeed)
+        With es
+            Walk += .Walk
+            Stride += .Stride
+            Run += .Run
+        End With
+    End Sub
+End Class
